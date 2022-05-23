834dac1b6499208574f1fb0cd9f8bf085e175ea9
Glory <nwaforglory6@gmail.com>
2022-05-23 16:26:26 +0100

    set up the admin section

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class LabouratoryAdmin(admin.ModelAdmin):
+	list_display=['user', 'lab_name', 'building_num', 'floor_num', 'capacity', 'total_pc', 'chairs', 'status']
+	prepopulated_fields={'slug': ('lab_name',)}
+
+class PCAdmin(admin.ModelAdmin):
+	list_display=['labouratory', 'pc_name', 'status']
+	prepopulated_fields={'slug': ('pc_name',)}
+
+class ReportPcAdmin(admin.ModelAdmin):
+	list_display=['pc', 'pc_problem', 'date', 'repaired']
+	
+
+admin.site.register(Labouratory, LabouratoryAdmin)
+admin.site.register(PC, PCAdmin)
+admin.site.register(ReportPc, ReportPcAdmin)
--- mainapp/models.py
+++ mainapp/models.py
@@ -15,20 +15,20 @@ class Labouratory(models.Model):
 	user=models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 	lab_name=models.CharField(max_length=250, help_text='Note:Labouratory name must be unique!!')
 	slug=models.SlugField(unique=True)
-	building_num=models.PositiveSmallIntegerField(help_text='Labouratory Building umber')
-	floor_num=models.PositiveSmallIntegerField(help_text='Labouratory floor number')
-	capacity=models.PositiveIntegerField(help_text='Labouratory capacity')
-	chairs=models.PositiveIntegerField(help_text='Number of chairs in the Labouratory')
-	chairs=models.PositiveIntegerField(help_text='Number of PCs in the Labouratory', default=0)
+	building_num=models.PositiveSmallIntegerField(help_text='Labouratory Building umber',default=0)
+	floor_num=models.PositiveSmallIntegerField(help_text='Labouratory floor number', default=1)
+	capacity=models.PositiveIntegerField(help_text='Labouratory capacity', default=0)
+	total_pc=models.PositiveIntegerField(help_text='Number of PCs in the Labouratory', default=0)
+	chairs=models.PositiveIntegerField(help_text='Number of chairs in the Labouratory',default=0)
 	status=models.CharField(max_length=20, choices=Labouratory_status)
 
 	def __str__(self):
 		return f"{self.lab_name} | {self.status}"
 
-	def save(self):
+	def save(self, *args, **kwargs):
 		if not self.slug:
 			self.slug=slugify(self.lab_name)
-			super(Labouratory, self).save(*args, **kwargs)
+		super(Labouratory, self).save(*args, **kwargs)
 
 class PC(models.Model):
 
@@ -36,18 +36,19 @@ class PC(models.Model):
 		('Repaired', 'Repaired'),
 		('Damaged', 'Damaged')
 	]
+	
 	labouratory=models.ForeignKey(Labouratory, on_delete=models.CASCADE)
 	pc_name=models.CharField(max_length=100)
 	slug=models.SlugField(unique=True)
 	status=models.CharField(max_length=10, choices=Pc_Status)
 
 	def __str__(self):
-		return f"{self.pc_name} | {self.status}"
+		return f"{self.pc_name} | {self.pc_name}"
 
-	def save(self):
+	def save(self, *args, **kwargs):
 		if not self.slug:
 			self.slug=slugify(self.pc_name)
-			super(PC, self).save(*args, Kwargs)
+		super(PC, self).save(*args, **kwargs)
 
 class ReportPc(models.Model):
 	pc=models.ForeignKey(PC, on_delete=models.CASCADE)
